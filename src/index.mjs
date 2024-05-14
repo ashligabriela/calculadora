f7b96a65c4c8cd1af27e0befe3f9e32c5327f411
ashligabriela <17041606@itdurango.edu.mx>
2024-05-13 18:27:38 -0600

    Cambios video de Miguel

--- src/index.mjs
+++ src/index.mjs
@@ -1,23 +1,12 @@
-// Obtener referencia a la div resultbox
-const resultBox = document.querySelector('.resultbox');
+const btnOne = document.getElementById("1");
+const inputBox = document.getElementsByClassName("resultbox")[0];
 
-// Funcion para manejar el clic en los botones
-const buttonClick = (event) => {
-    const clickedButtonValue = event.target.textContent;
-    console.log("Button clicked:", clickedButtonValue);
-
-    // Obtener el contenido actual de la resultbox
-    let currentResult = resultBox.textContent;
-
-    // Concatenar el valor del boton clicado al contenido actual de la resultbox
-    currentResult += clickedButtonValue;
-    // console.log("Current result:", currentResult);
-
-    // Actualizar el contenido de la resultbox con el nuevo valor
-    resultBox.textContent = currentResult;
-};
-
-// Seleccionar todos los botones y agregarles un event listener
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', buttonClick);
-});
+function onClikc(evento) {
+  const btnValue = evento.target.textContent;
+  if (inputBox.textContent === "0") {
+    inputBox.textContent = btnValue;
+  } else {
+    inputBox.textContent = inputBox.textContent + btnValue;
+  }
+}
+btnOne.addEventListener("click", onClikc);
