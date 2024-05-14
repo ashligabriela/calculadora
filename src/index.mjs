bc49182a23719a133fa93eb2040ea0c19ff6bd8c
ashligabriela <17041606@itdurango.edu.mx>
2024-05-13 18:40:55 -0600

    Ejercicio de poner el 0 en el display

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,19 @@
 const btnOne = document.getElementById("1");
 const inputBox = document.getElementsByClassName("resultbox")[0];
+const btnClean = document.getElementById("clean");
 
 function onClikc(evento) {
   const btnValue = evento.target.textContent;
+
+  if (evento.target === btnClean) {
+    inputBox.textContent = "0";
+  }else{
   if (inputBox.textContent === "0") {
     inputBox.textContent = btnValue;
   } else {
     inputBox.textContent = inputBox.textContent + btnValue;
   }
 }
+}
 btnOne.addEventListener("click", onClikc);
+btnClean.addEventListener("click",onClikc);
